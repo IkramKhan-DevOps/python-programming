e0e3d9ba4699f6558e044bdcbe7acbb5682182c5
Misbah Ur Rehman <hybridoitc@gmail.com>
2021-08-12 18:59:36 +0500

    practice_4 sequences uploaded

---
+++ practice_4_1_sequences.py
@@ -0,0 +1,22 @@
+
+
+"""
+   SEQUENCES LECTURE
+   =================
+              1. LIST         2. TUPLE            3.SET      4.DICTIONARY
+--------------------------------------------------------------------------
+Id            []               ()                 {}
+order         Ordered          Ordered            Un-ordered
+change        Changeable       Unchangeable       --
+duplication   Duplicate        Duplicate          Un-duplicate
+indexing      indexing         indexing           Un-indexed
+-------------------------------------------------------------------------
+keyword       list             tuple              set
+len           len()            len()              len()
+datatype      any, mixture     any, mixture       any, mixture
+init          [],list(())      (), tuple(())      set(())
+extras        --               single=>(value,)   {}=>means dict
+--------------------------------------------------------------------------
+
+"""
+print(2 in [1, 2, 3, 4, 5, 6, 7])
